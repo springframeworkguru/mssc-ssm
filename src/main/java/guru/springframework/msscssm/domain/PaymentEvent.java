f54d1961162ba07cb291fd5332e84940830acf24
John Thompson <john@springframework.guru>
2019-07-23 18:15:11 -0400

    Adding enums for states and events

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * Created by jt on 2019-07-23.
+ */
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentState.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * Created by jt on 2019-07-23.
+ */
+public enum PaymentState {
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
+}
