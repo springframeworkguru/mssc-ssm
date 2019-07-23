1609695f23f071e067e14a1204f341c4da74fcb1
John Thompson <john@springframework.guru>
2019-07-23 18:42:38 -0400

    adding state machine config for states

---
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+/**
+ * Created by jt on 2019-07-23.
+ */
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+}
