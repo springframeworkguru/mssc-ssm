d249938148c6a55bdb99b5f0793d25ff665202d8
John Thompson <john@springframework.guru>
2019-08-10 15:08:15 -0400

    adding payment service

---
+++ src/main/java/guru/springframework/msscssm/services/PaymentService.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+/**
+ * Created by jt on 2019-08-10.
+ */
+public interface PaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+}
---
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -0,0 +1,41 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 2019-08-10.
+ */
+@RequiredArgsConstructor
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(PaymentState.NEW);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        return null;
+    }
+}
